98c5e6a1022382ffbd8ccdaa9f0c090c54ce586b
cash <cash@cashwu.com>
2017-09-05 09:14:39 +0800

    input ABC 1 and A test case

--- Kata20170905_HelpTheBookseller/HelpTheBooksellerTests.cs
+++ Kata20170905_HelpTheBookseller/HelpTheBooksellerTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170905_HelpTheBookseller
     public class HelpTheBooksellerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_ABC_1_and_A_should_return_A_1()
         {
+            StockSummaryShouldBe("(A : 1)", new[] { "ABC 1" }, new[] { "A" });
+        }
+
+        private static void StockSummaryShouldBe(string expected, string[] lstOfArt, string[] lstOf1StLetter)
+        {
+            var stockList = new StockList();
+            var actual = stockList.stockSummary(lstOfArt, lstOf1StLetter);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class StockList
+    {
+        public string stockSummary(string[] lstOfArt, string[] lstOf1stLetter)
+        {
+            return "(A : 1)";
         }
     }
 }
